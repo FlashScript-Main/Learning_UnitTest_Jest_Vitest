e23b2a2a79d9619b903d1e82ef5f7b42d1400ce2
FlashScript <itzflashscript@gmail.com>
2024-11-28 15:24:07 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// index.ts
 
-function App() {
-  const [count, setCount] = useState(0)
+/*
+    🔹 let us understand what exactly is an automated test?
+    we know that an automated test is a piece of code to test the software code
+    but what is that piece of code?
+    well an automated test is code that throws an error 
+    when the actual output does NOT match the expected output 🔹
+*/
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const getFullName = (firstName: string, lastName: string) => {
+    return `${firstName} ${lastName}`
+}
+
+const actualFullName = getFullName("Bruce", "Wayne");
+const expectedFullName = "BruceWayne";
+
+if (actualFullName !== expectedFullName) {
+    throw new Error(`${actualFullName} is NOT equal to ${expectedFullName}`);
+}
+
+/*
+    🔹 this is exactly what Jest is going to help us with.
+    an automated test is code that throws an error 
+    when the actual output does not match the expected output
+    it will pass when the output is correct
+    and provide meaningful feedback when it is NOT 🔹
+*/
+
+
+const App = () => {
+
+    
+    return (
+        <div>App</div>
+    )
+
 }
 
 export default App
