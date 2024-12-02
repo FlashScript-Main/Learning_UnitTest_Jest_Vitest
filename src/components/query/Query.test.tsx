f66d176280f6cb1ec3882269c1f5508c82f9b6e5
FlashScript <itzflashscript@gmail.com>
2024-12-02 15:04:31 +0330

    React Testing Library | Queries

--- src/components/query/Query.test.tsx
+++ src/components/query/Query.test.tsx
@@ -1,1 +1,24 @@
 // Root 👉🏻 "src" Folder 👉🏻 "components" Folder 👉🏻 "query" Folder 👉🏻 Query.test.tsx
+
+import { describe, expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Query from "./Query";
+
+describe("Query", () => {
+    test("renders correctly", () => {
+        render(<Query />);
+
+        const nameInput = screen.getByRole("textbox");
+        expect(nameInput).toBeInTheDocument();
+
+        const jobLocationSelect = screen.getByRole("combobox");
+        expect(jobLocationSelect).toBeInTheDocument();
+
+        const termsCheckbox = screen.getByRole("checkbox");
+        expect(termsCheckbox).toBeInTheDocument();
+
+        // Let's check if the Submit Button Renders Correctly
+        const submitButton = screen.getByRole("button");
+        expect(submitButton).toBeInTheDocument();
+    });
+});
